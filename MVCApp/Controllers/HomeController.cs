5fa6a74b34e11e8ba560e96bf3179aedf812e9fd
gordonem <elijah.gordon94@gmail.com>
2018-05-10 15:30:03 -0400

    Needs Data Validation for StateDropDown, field can be left blank at the moment.

---
+++ MVCApp/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCApp.Models;
+
+namespace MVCApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            string timeOfDay;
+            if (hour < 12)
+                timeOfDay = "Good Morning";
+            else if (hour < 18)
+                timeOfDay = "Good Afternoon";
+            else
+                timeOfDay = "Good Evening";
+            ViewBag.Greeting = timeOfDay;
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult InfoForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult InfoForm(Response response)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Thanks", Response);
+            }
+            else
+            {
+                {
+                    return View();
+                }
+            }
+        }
+    }
+}
---
+++ MVCApp/Models/Response.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.UI.WebControls;
+
+namespace MVCApp.Models
+{
+    public enum StateDropDown
+    {
+        [Display(Name="Please Select a State")]
+        SelectState,
+        AK,
+        AL,
+        AR,
+        AZ,
+        CA,
+        CO,
+        CT,
+        DE,
+        FL,
+        GA,
+        HI,
+        IA,
+        ID,
+        IL,
+        IN,
+        KS,
+        KY,
+        LA,
+        MA,
+        MD,
+        ME,
+        MI,
+        MN,
+        MO,
+        MS,
+        MT,
+        NC,
+        ND,
+        NE,
+        NH,
+        NJ,
+        NM,
+        NV,
+        NY,
+        OH,
+        OK,
+        OR,
+        PA,
+        RI,
+        SC,
+        SD,
+        TN,
+        TX,
+        UT,
+        VA,
+        VT,
+        WA,
+        WI,
+        WV,
+        WY
+    }
+    public class Response
+    {
+        [Required(ErrorMessage = "Please enter your First Name")]
+        [StringLength(25, ErrorMessage = "First Name cannot exceed 25 characters!")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Please enter your Last Name")]
+        [StringLength(50, ErrorMessage = "Last Name cannot exceed 50 characters!")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Please enter your Street Address")]
+        public string StreetAddress { get; set; }
+        
+        //Suit/Apartment number field is not required
+        public string SuitApartmentNum { get; set; }
+
+        [Required(ErrorMessage = "Please enter your City")]
+        [StringLength(75, ErrorMessage = "City name cannot exceed 75 characters!")]
+        public string City { get; set; }
+
+        []
+        public StateDropDown State { get; set; }
+
+        [Required(ErrorMessage = "Please enter your Zip Code")]
+        [StringLength(10, MinimumLength = 5, ErrorMessage = "Please enter a valid Zip Code")]
+        public string ZipCode { get; set; }
+
+        [Required(ErrorMessage = "Please enter your email address")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Please enter a valid email address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please enter your phone number")]
+        public string PhoneNum { get; set; }
+
+
+    }
+}
